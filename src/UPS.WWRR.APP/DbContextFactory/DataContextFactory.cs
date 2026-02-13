5fcd279b5b0b48183db491f42bbf102fbe888797
Shashidhar Reddy Kotha <sreddykotha@ups.com>
2026-02-13 01:21:51 +0000

    Merged PR 576791: Updated DataContextFactory.cs
    Updated DataContextFactory.cs

--- src/UPS.WWRR.APP/DbContextFactory/DataContextFactory.cs
+++ src/UPS.WWRR.APP/DbContextFactory/DataContextFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using System.Diagnostics.CodeAnalysis;
 using UPS.WWRR.Data.Models;
 using UPS.WWRR.Data.Common;
+using UPS.WWRR.Business.Common.Helper;
 
 namespace UPS.WWRR.API.DbContextFactory
 {
@@ -13,12 +14,26 @@ namespace UPS.WWRR.API.DbContextFactory
         public DataContext CreateDbContext(string[] args)
         {
             var connectionString = Environment.GetEnvironmentVariable("ALLOYDB_CONNECTION")
-                        ?? throw new InvalidOperationException("ALLOYDB_CONNECTION environment variable is not set.");
-            
+                ?? throw new InvalidOperationException("ALLOYDB_CONNECTION environment variable is not set.");
+
+            // Parse host, db, user from connection string
+            var (db_host, database, iamDbUser) = PgDataSourceFactory.Parse(connectionString);
+
+            // Create DataSource with IAM token provider
+            var dataSource = PgDataSourceFactory
+                .CreateAsync(db_host, database, iamDbUser, requireSsl: true)
+                .GetAwaiter()
+                .GetResult();
+
             var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+
             optionsBuilder
-                .UseNpgsql(connectionString, npgSqlOptions =>
-                    npgSqlOptions.MigrationsHistoryTable("__EFMigrationsHistory", DataConstants.defaultSchema))
+                .UseNpgsql(
+                    dataSource,
+                    npgSqlOptions =>
+                        npgSqlOptions.MigrationsHistoryTable(
+                            "__EFMigrationsHistory",
+                            DataConstants.defaultSchema))
                 .LogTo(Console.WriteLine, LogLevel.Information)
                 .EnableDetailedErrors();
 
