be01fb0d3dfa477dd1fb8218194a1456080b632b
Michael Landau <mlandau@ups.com>
2026-03-30 15:16:15 +0000

    Merged PR 674433: Modify TVASYLN Data Load to Allow Empty String Values for GPN Columns
    changed tvasyln columns org_gpn_mnm_te and dtn_gpn_mnm_te to allow empty strings
    
    Related work items: #6695293, #6695582

--- src/UPS.WWRR.Business/DTO/Models/LoadTableDto/ValidAccessorialLaneDto.cs
+++ src/UPS.WWRR.Business/DTO/Models/LoadTableDto/ValidAccessorialLaneDto.cs
@@ -42,10 +42,10 @@ namespace UPS.WWRR.Business.DTO.Models.LoadTableDto
         [Required]
         public DateTime REC_EFF_END_DT { get; set; }
 
-        [Required, StringLength(4)]
+        [Required(AllowEmptyStrings = true), StringLength(4)]
         public string ORG_GPN_MNM_TE { get; set; } = string.Empty;
 
-        [Required, StringLength(4)]
+        [Required(AllowEmptyStrings = true), StringLength(4)]
         public string DTN_GPN_MNM_TE { get; set; } = string.Empty;
     }
 }
